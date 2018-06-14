027b6c65aa67700c464f823d23e443b0fded2f54
50boc <obarbier13@gmail.com>
2018-06-13 22:00:23 -0400

    initial submit

---
+++ python/app.py
@@ -0,0 +1,26 @@
+from flask import Flask , jsonify
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+
+app = Flask(__name__)
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_task():
+	return jsonify({'tasks':tasks})
+
+if __name__=='__main__':
+	app.run(debug=True)
