d90d309c88cd638d55961a847845d25b45a63431
50boc <obarbier13@gmail.com>
2018-06-17 22:00:44 -0400

    test

--- python/app.py
+++ python/app.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask , jsonify
+app = Flask(__name__)
 
 tasks = [
     {
@@ -23,4 +25,5 @@ def get_task():
 	return jsonify({'tasks':tasks})
 
 if __name__=='__main__':
-	app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
